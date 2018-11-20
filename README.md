62173e186357045758c9c5a57d5712743ce47c37
juancarlospaco <juancarlospaco@gmail.com>
2018-11-20 12:04:54 -0300

    Init

--- README.md
+++ README.md
@@ -1,2 +1,146 @@
-# nim-open-elevation
-OpenStreetMap Elevation API MultiSync Client for Nim
+# Nim-Open-Elevation
+
+- [Open Elevation](https://open-elevation.com) for [OpenStreetMap](https://openstreetmap.org) MultiSync API Lib and App.
+
+
+# Install
+
+- `nimble install openelevation`
+
+
+# Use
+
+```nim
+import asyncdispatch, json, openelevation
+
+let sample_query = %*{
+  "locations":
+  [
+    {
+      "latitude": 10.55,
+      "longitude": 10.75
+    },
+    {
+      "latitude": 20.42,
+      "longitude": 20.5
+    },
+    {
+      "latitude": 41.161758,
+      "longitude": -8.583933
+    }
+  ]
+}  # Example query taken from the Documentation.
+
+## Sync client.
+let client = OpenElevation(timeout: 99.byte)
+echo client.lookup(42.0, 50.0)              # GET  (1 query max)
+echo client.lookup(locations=sample_query)  # POST (multiple queries)
+
+## Async client.
+proc async_oe() {.async.} =
+  let
+    async_client = AsyncOpenElevation(timeout: 99.byte)
+    async_response = await async_client.lookup(42.0, 50.0)
+  echo async_response.pretty
+
+wait_for async_oe()
+
+# Check the Docs for more API Calls...
+```
+
+
+# Command Line App
+
+- For Uglyfied JSON use ``--ugly`` (does not reduce bandwith usage).
+- The App supports English and Spanish.
+
+**1 Query:**
+
+```bash
+./open_elevation --color --lower --timeout=9 --lat=42.5 --lon=55.75
+```
+
+**Multiple Queries:**
+
+```bash
+./open_elevation --color --lower --timeout=9 '{"locations":[{"latitude":9,"longitude":10},{"latitude":41.68,"longitude":-8.58}]}'
+```
+
+# API
+
+- API Calls use [the DoNotTrack HTTP Header.](https://en.wikipedia.org/wiki/Do_Not_Track)
+- The `timeout` argument is on Seconds.
+- For Proxy support define a `proxy` of `Proxy` type.
+- No OS-specific code, so it should work on Linux, Windows and Mac. Not JS.
+- Run the module itself for an Example.
+- Run `nim doc open_elevation.nim` for more Documentation.
+
+
+# FAQ
+
+
+<details>
+  <summary>Click to see FAQ</summary>
+
+- Yo solo hablo Espanol ?
+
+La aplicacion de linea de comandos soporta Espanol.
+
+- This works without SSL ?.
+
+No.
+
+- This works with SSL ?.
+
+Yes.
+
+- This works with Asynchronous code ?.
+
+Yes.
+
+- This works with Synchronous code ?.
+
+Yes.
+
+- This requires API Key or Login ?.
+
+No.
+
+- This requires Credit Card or Payments ?.
+
+No.
+
+- Can I use the OpenStreetMap data ?.
+
+Yes. [**You MUST give Credit to OpenStreetMap Contributors!.**](https://wiki.openstreetmap.org/wiki/Legal_FAQ#3a._I_would_like_to_use_OpenStreetMap_maps._How_should_I_credit_you.3F)
+
+- How to Uglify the output?
+
+`--ugly`
+
+- How to Lowercase the output?
+
+`--lower`
+
+- How to set a Timeout ?
+
+`--timeout=99`
+
+- How to set the Coordinates ?
+
+`--lat=15.42 --lon=12.75`
+
+- How to use colors ?
+
+`--color`
+
+- How to see the Help ?
+
+`--help`
+
+</details>
+
+
+# Requisites
+
+- None.
---
+++ src/open_elevation.nim
@@ -0,0 +1,99 @@
+
+import asyncdispatch, httpclient, strutils, json
+{.passL: "-s".}
+const open_elevation_url* = "https://api.open-elevation.com/api/v1/lookup" ## Open Elevation API URL
+
+type
+  OpenElevationBase*[HttpType] = object ## Base object.
+    timeout*: byte  ## Timeout Seconds for API Calls, byte type, 0~255.
+    proxy*: Proxy  ## Network IPv4 / IPv6 Proxy support, Proxy type.
+  OpenElevation* = OpenElevationBase[HttpClient]           ##  Sync Open Elevation API Client.
+  AsyncOpenElevation* = OpenElevationBase[AsyncHttpClient] ## Async Open Elevation API Client.
+
+template clientify(this: OpenElevation | AsyncOpenElevation): untyped =
+  ## Build & inject basic HTTP Client with Proxy and Timeout.
+  var client {.inject.} =
+    when this is AsyncOpenElevation: newAsyncHttpClient(
+      proxy = when declared(this.proxy): this.proxy else: nil, userAgent="")
+    else: newHttpClient(
+      timeout = when declared(this.timeout): this.timeout.int * 1_000 else: -1,
+      proxy = when declared(this.proxy): this.proxy else: nil, userAgent="")
+  client.headers = newHttpHeaders({"dnt": "1", "accept":
+    "application/json", "content-type": "application/json"})
+
+proc lookup*(this: OpenElevation | AsyncOpenElevation, lat, lon: float32): Future[JsonNode] {.multisync.} =
+  ## Open Elevation GET Endpoint for simple query of individual points (lat, lon).
+  clientify(this)
+  let
+    url = open_elevation_url & "?locations=" & $lat & "," & $lon
+    responses =
+      when this is AsyncOpenElevation: await client.get(url=url)
+      else: client.get(url=url)
+  result = parse_json(await responses.body)
+
+proc lookup*(this: OpenElevation | AsyncOpenElevation, locations: JsonNode): Future[JsonNode] {.multisync.} =
+  ## Open Elevation POST Endpoint for bulk query of several points (locations JSON).
+  assert locations.hasKey("locations"), "locations must have a 'locations' array"
+  assert locations["locations"].len > 0, "locations must be a non-empty array"
+  clientify(this)
+  let
+    responses =
+      when this is AsyncOpenElevation: await client.post(url=open_elevation_url, body= $locations)
+      else: client.post(url=open_elevation_url, body= $locations)
+  result = parse_json(await responses.body)
+
+
+when is_main_module and not defined(js):
+  import parseopt, terminal, random
+  {.optimization: size.}
+  const helpy = """
+  Get the Elevation in Meters of any specific place in the world (0=Sea level),
+  using the Open Elevation for OpenStreetMap API online services.
+
+  For Uglyfied JSON use --ugly (does not reduce bandwith usage).
+  This requires at least basic skills with JSON and OpenStreeMap.
+  For more information and help check the Documentation.
+
+  Para JSON Minificado afeado usar --fea (no reduce uso de ancho de banda).
+  Requiere por lo menos conocimientos basicos de JSON y OpenStreeMap.
+  Para mas informacion y ayuda ver la Documentacion.
+
+  👑 https://github.com/juancarlospaco/nim-open-elevation#nim-open-elevation 👑
+
+  Use:
+  ./open_elevation --color --lower --timeout=9 --lat=42.5 --lon=55.75
+  ./open_elevation --color --lower --timeout=9 '{"locations":[{"latitude":9,"longitude":10},{"latitude":41.68,"longitude":-8.58}]}'
+
+  Uso (Spanish):
+  ./open_elevation --color --minusculas --timeout=9 --lat=42.5 --lon=55.75
+  ./open_elevation --color --minusculas --timeout=9 '{"locations":[{"latitude":9,"longitude":10},{"latitude":41.68,"longitude":-8.58}]}'
+  """
+  var
+    lat, lon: float32
+    taimaout = 99.byte
+    minusculas, fea: bool
+    json_query: JsonNode
+  for tipoDeClave, clave, valor in getopt():
+    case tipoDeClave
+    of cmdShortOption, cmdLongOption:
+      case clave
+      of "version":                      quit("0.1.5", 0)
+      of "license", "licencia":          quit("MIT", 0)
+      of "help", "ayuda":                quit(helpy, 0)
+      of "minusculas", "lower":          minusculas = true
+      of "ugly", "fea":                  fea = true
+      of "timeout":                      taimaout = valor.parseInt.byte # HTTTP Timeout.
+      of "lat", "latitude", "latitud":   lat = valor.parseFloat.float32
+      of "lon", "longitude", "longitud": lon = valor.parseFloat.float32
+      of "color":
+        randomize()
+        setBackgroundColor(bgBlack)
+        setForegroundColor([fgRed, fgGreen, fgYellow, fgBlue, fgMagenta, fgCyan, fgWhite].rand)
+    of cmdArgument:
+      json_query = clave.string.parseJson
+    of cmdEnd: quit("Wrong Parameters, see Help with --help", 1)
+  let
+    clientito = OpenElevation(timeout: taimaout)
+    respuesta = if json_query != nil: clientito.lookup(json_query) else: clientito.lookup(lat, lon)
+    resultadito = if fea: $respuesta else: respuesta.pretty
+  if minusculas: echo resultadito.toLowerAscii else: echo resultadito
